07185688cf936d1b344fd600cdc16841d5676f04
Jason McVetta <jason.mcvetta@gmail.com>
2012-07-15 05:17:11 -0700

    Remove !goci build directive

--- neo4j_test.go
+++ neo4j_test.go
@@ -1,9 +1,6 @@
 // Copyright (c) 2012 Jason McVetta.  This is Free Software, released under the 
 // terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
 
-// +build !goci
-
-
 package neo4j
 
 import (
