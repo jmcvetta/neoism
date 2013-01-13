f2531d1dc3f76bee389f8b245cc3401b99f0b14a
Jason McVetta <jason.mcvetta@gmail.com>
2013-01-13 12:21:38 -0800

    Tests for Create Node.

---
+++ node_test.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2012 Jason McVetta.  This is Free Software, released under the 
+// terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
+
+//
+// The Neo4j Manual section numbers quoted herein refer to the manual for 
+// milestone release 1.8.  http://docs.neo4j.org/chunked/1.8/
+
+package neo4j
+
+import (
+	"github.com/bmizerany/assert"
+	// "github.com/jmcvetta/randutil"
+	// "log"
+	// "sort"
+	"testing"
+)
+
+// 18.4.1. Create Node
+func TestCreateNode(t *testing.T) {
+	// Create
+	n0, err := db.Nodes.Create(emptyProps)
+	if err != nil {
+		t.Error(err)
+	}
+	// Confirm creation
+	_, err = db.Nodes.Get(n0.Id())
+	if err != nil {
+		t.Error(err)
+	}
+	// Cleanup
+	n0.Delete()
+}
+
+// 18.4.2. Create Node with properties
+func TestCreateNodeWithProperties(t *testing.T) {
+	// Create
+	props0 := Properties{}
+	props0["foo"] = "bar"
+	props0["spam"] = "eggs"
+	n0, err := db.Nodes.Create(props0)
+	if err != nil {
+		t.Error(err)
+	}
+	// Confirm creation
+	_, err = db.Nodes.Get(n0.Id())
+	if err != nil {
+		t.Error(err)
+	}
+	// Confirm properties
+	props1, _ := n0.Properties()
+	assert.Equalf(t, props0, props1, "Node properties not as expected")
+	// Cleanup
+	n0.Delete()
+}
