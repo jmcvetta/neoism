895718482d1325faa0fb8a597f06bf705221218d
Jason McVetta <jason.mcvetta@gmail.com>
2013-01-25 15:21:16 -0800

    Use serviceRoot to populate Database struct fields, rather than being included as a field itself.

--- database.go
+++ database.go
@@ -16,22 +16,31 @@ type Database struct {
 	url           *url.URL // Root URL for REST API
 	client        *http.Client
 	rc            *restclient.Client
-	info          *serviceRoot
 	Nodes         *NodeManager
 	Relationships *RelationshipManager
+	//
+	HrefNode       string
+	HrefRefNode    string
+	HrefNodeIndex  string
+	HrefRelIndex   string
+	HrefExtInfo    string
+	HrefRelTypes   string
+	HrefBatch      string
+	HrefCypher     string
+	Version    string
 }
 
 // A serviceRoot describes services available on the Neo4j server
 type serviceRoot struct {
 	Extensions interface{} `json:"extensions"`
-	Node       string      `json:"node"`
-	RefNode    string      `json:"reference_node"`
-	NodeIndex  string      `json:"node_index"`
-	RelIndex   string      `json:"relationship_index"`
-	ExtInfo    string      `json:"extensions_info"`
-	RelTypes   string      `json:"relationship_types"`
-	Batch      string      `json:"batch"`
-	Cypher     string      `json:"cypher"`
+	HrefNode       string      `json:"node"`
+	HrefRefNode    string      `json:"reference_node"`
+	HrefNodeIndex  string      `json:"node_index"`
+	HrefRelIndex   string      `json:"relationship_index"`
+	HrefExtInfo    string      `json:"extensions_info"`
+	HrefRelTypes   string      `json:"relationship_types"`
+	HrefBatch      string      `json:"batch"`
+	HrefCypher     string      `json:"cypher"`
 	Version    string      `json:"neo4j_version"`
 }
 
@@ -41,7 +50,6 @@ func Connect(uri string) (*Database, error) {
 	db := &Database{
 		client: new(http.Client),
 		rc:     restclient.New(),
-		info:   &sr,
 	}
 	u, err := url.Parse(uri)
 	if err != nil {
@@ -57,31 +65,41 @@ func Connect(uri string) (*Database, error) {
 		db:      db,
 		Indexes: &RelationshipIndexManager{},
 	}
-	r := restclient.RestRequest{
+	req := restclient.RestRequest{
 		Url:    u.String(),
 		Method: restclient.GET,
 		Result: &sr,
 		Error:  &e,
 	}
-	status, err := db.rc.Do(&r)
+	status, err := db.rc.Do(&req)
 	if err != nil {
+		logPretty(req)
 		log.Println(e.Message)
 		log.Println(e.Exception)
 		log.Println(e.Stacktrace)
 		return db, err
 	}
 	switch {
-	case status == 200 && db.info.Version != "":
-		// Set HrefIndex so the generic indexManager knows what URL to use when
-		// creating a NodeIndex.
-		db.Nodes.Indexes.HrefIndex = db.info.NodeIndex
-		db.Relationships.Indexes.HrefIndex = db.info.RelIndex
-		return db, nil // Success!
 	case status == 404:
 		return db, InvalidDatabase
+	case status != 200 || sr.Version == "":
+		logPretty(req)
+		return db, BadResponse
 	}
-	log.Println(e.Message)
-	log.Println(e.Exception)
-	log.Println(e.Stacktrace)
-	return db, BadResponse
-}
+	// Populate Database struct
+	db.HrefNode = sr.HrefNode
+	db.HrefRefNode = sr.HrefRefNode
+	db.HrefNodeIndex = sr.HrefNodeIndex
+	db.HrefRelIndex = sr.HrefRelIndex
+	db.HrefExtInfo = sr.HrefExtInfo
+	db.HrefRelTypes = sr.HrefRelTypes
+	db.HrefBatch = sr.HrefBatch
+	db.HrefCypher = sr.HrefCypher
+	db.Version = sr.Version
+	// Set HrefIndex so the generic indexManager knows what URL to use when
+	// creating a NodeIndex.
+	db.Nodes.Indexes.HrefIndex = sr.HrefNodeIndex
+	db.Relationships.Indexes.HrefIndex = sr.HrefRelIndex
+	// Success!
+	return db, nil
+	}
