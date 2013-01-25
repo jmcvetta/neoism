b063d05822fc7c3c8a17fb076e97ffdf44f0840b
Jason McVetta <jason.mcvetta@gmail.com>
2013-01-25 14:22:52 -0800

    Informative comment on test.go

--- test.go
+++ test.go
@@ -8,6 +8,20 @@ for milestone release 1.8.  http://docs.neo4j.org/chunked/1.8/
 
 *******************************************************************************/
 
+/*
+
+To run these tests, a Neo4j 1.8 instance must be running on localhost on the
+default port.  
+
+If the test suite complete successfully, all new nodes & relationships created
+for testing will have been deleted in cleanup.  However if the test suite fails
+to complete due to a panic, the cleanup code may not get called.  Therefore it
+is not recommended to run this test suite on a db containing valuable data - run
+it on a throwaway testing db instead! It's possible we could reduce this risk by
+using defer() for cleanup.
+
+*/
+
 package neo4j
 
 import (
