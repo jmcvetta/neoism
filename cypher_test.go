5e9d27fab3bf5547a586f63ccef30348eb2c59d6
Jason McVetta <jason.mcvetta@gmail.com>
2013-01-29 14:42:29 -0800

    Test for 18.3.1. Send queries with parameters

--- cypher_test.go
+++ cypher_test.go
@@ -4,33 +4,42 @@
 package neo4j
 
 import (
+	"github.com/bmizerany/assert"
 	"testing"
-	// "fmt"
 )
 
 // 18.3.1. Send queries with parameters
 func TestCypherSendQueryWithParameters(t *testing.T) {
 	// Create
-	// idx0, _ := db.Nodes.Indexes.CreateWithConf("name_auto_index", "fulltext", "lucene")
+	idx0, _ := db.Nodes.Indexes.Create("name_index")
 	n0, _ := db.Nodes.Create(Properties{"name": "I"})
+	idx0.Add(n0, "name", "I")
 	n1, _ := db.Nodes.Create(Properties{"name": "you"})
+	// idx0.Add(n1, "name", "you")
 	r0, _ := n0.Relate("know", n1.Id(), nil)
+	r1, _ := n0.Relate("love", n1.Id(), nil)
 	// Deferred Cleanup
+	defer idx0.Delete()
 	defer r0.Delete()
+	defer r1.Delete()
 	defer n0.Delete()
 	defer n1.Delete()
-	// defer idx0.Delete()
 	// Query
-	query := "START x = node:node_auto_index(name={startName}) MATCH path = (x-[r]-friend) WHERE friend.name = {name} RETURN TYPE(r)"
-	// query := fmt.Sprintf("START n=node(%v) RETURN n", n0.Id())
+	query := "START x = node:name_index(name={startName}) MATCH path = (x-[r]-friend) WHERE friend.name = {name} RETURN TYPE(r)"
 	params := map[string]string{
-		// "startName": "I",
-		// "name":      "you",
+		"startName": "I",
+		"name":      "you",
 	}
 	result, err := db.Cypher(query, params)
 	if err != nil {
 		t.Error(err)
 	}
 	// Check result
-	logPretty(result)
+	expCol := []string{"TYPE(r)"}
+	// Our test only passes if Neo4j returns "know" and "love" in this order.  Is
+	// there any guarantee about order?  Can we modify the query to ensure order? 
+	// Or is there a convenient way to sort result.Data here before checking it?
+	expDat := [][]string{[]string{"know"}, []string{"love"}}
+	assert.Equal(t, expCol, result.Columns)
+	assert.Equal(t, expDat, result.Data)
 }
