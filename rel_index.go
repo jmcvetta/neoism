cbc90b6df51fba894f96e5999b328deb01cef4c4
Jason McVetta <jason.mcvetta@gmail.com>
2013-07-15 17:29:12 -0700

    Revert "remove relationship indexes"
    This reverts commit 628e3c93a7fb6418651d723eebfb5df2b94e23f5.

---
+++ rel_index.go
@@ -0,0 +1,49 @@
+// Copyright (c) 2012-2013 Jason McVetta.  This is Free Software, released under
+// the terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
+// Resist intellectual serfdom - the ownership of ideas is akin to slavery.
+
+package neo4j
+
+// A RelationshipIndex is an index for searching Relationships.
+type RelationshipIndex struct {
+	index
+}
+
+// CreateRelIndex creates a new relationship index with optional type and
+// provider.
+func (db *Database) CreateRelIndex(name, idxType, provider string) (*RelationshipIndex, error) {
+	idx, err := db.createIndex(db.HrefRelIndex, name, idxType, provider)
+	if err != nil {
+		return nil, err
+	}
+	return &RelationshipIndex{*idx}, nil
+}
+
+// RelIndexes returns all relationship indexes.
+func (db *Database) RelIndexes() ([]*RelationshipIndex, error) {
+	indexes, err := db.indexes(db.HrefRelIndex)
+	if err != nil {
+		return nil, err
+	}
+	ris := make([]*RelationshipIndex, len(indexes))
+	for i, idx := range indexes {
+		ris[i] = &RelationshipIndex{*idx}
+	}
+	return ris, nil
+}
+
+// RelIndex returns the named relationship index.
+func (db *Database) RelIndex(name string) (*RelationshipIndex, error) {
+	idx, err := db.index(db.HrefRelIndex, name)
+	if err != nil {
+		return nil, err
+	}
+	ri := RelationshipIndex{*idx}
+	return &ri, nil
+}
+
+// Remove deletes all entries with a given node, key and value from the index.
+// If value or both key and value are the blank string, they are ignored.
+func (rix *RelationshipIndex) Remove(r *Relationship, key, value string) error {
+	return rix.remove(r, key, value)
+}
---
+++ rel_index_test.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2012-2013 Jason McVetta.  This is Free Software, released under
+// the terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
+// Resist intellectual serfdom - the ownership of ideas is akin to slavery.
+
+package neo4j
+
+import (
+	"github.com/bmizerany/assert"
+	"testing"
+)
+
+// The underlying functions are used for node and relationship indexes.  For
+// now we will test only the pieces of code that are relationship-specific.
+func TestRelationshipIndexes(t *testing.T) {
+	db := connectTest(t)
+	name := rndStr(t)
+	//
+	// Create new index
+	//
+	idx0, err := db.CreateRelIndex(name, "", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer idx0.Delete()
+	assert.Equal(t, idx0.Name, name)
+	//
+	// Get the index we just created
+	//
+	idx1, err := db.RelIndex(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, idx0.Name, idx1.Name)
+	//
+	// See if we get this index, and only this index
+	//
+	indexes, err := db.RelIndexes()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, 1, len(indexes))
+	idx2 := indexes[0]
+	assert.Equal(t, idx0.Name, idx2.Name)
+}
