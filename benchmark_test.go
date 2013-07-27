1ff0afc3ac6f51664d591e830f739ace2788d4d4
Jason McVetta <jason.mcvetta@gmail.com>
2013-07-27 15:41:55 -0700

    BenchmarkNodeChainBatch

--- benchmark_test.go
+++ benchmark_test.go
@@ -18,19 +18,77 @@ func connectBench(b *testing.B) *Database {
 	return db
 }
 
-// 18.5.2. Create relationship
 func BenchmarkNodeChain(b *testing.B) {
 	b.StopTimer()
 	db := connectBench(b)
 	b.StartTimer()
-	n0, _ := db.CreateNode(Props{})
+	n0, _ := db.CreateNode(Props{"name": 0})
 	defer n0.Delete()
 	lastNode := n0
-	for i := 0; i < b.N; i++ {
-		nextNode, _ := db.CreateNode(Props{})
+	for i := 1; i < b.N; i++ {
+		nextNode, _ := db.CreateNode(Props{"name": i})
 		defer nextNode.Delete()
-		r0, _ := lastNode.Relate("knows", nextNode.Id(), Props{})
+		r0, _ := lastNode.Relate("knows", nextNode.Id(), Props{"name": i})
 		defer r0.Delete()
 	}
 	b.StopTimer()
+}
+
+func BenchmarkNodeChainBatch(b *testing.B) {
+	b.StopTimer()
+	db := connectBench(b)
+	b.StartTimer()
+	qs := []*CypherQuery{}
+	nodes := []int{}
+	rels := []int{}
+	cq := CypherQuery{
+		Statement:  `CREATE (n:Person {name: {i}}) RETURN n`,
+		Parameters: Props{"i": 0},
+		Result:     &[]Node{},
+	}
+	qs = append(qs, &cq)
+	nodes = append(nodes, 0)
+	for i := 1; i < b.N; i++ {
+		cq0 := CypherQuery{
+			Statement:  `CREATE (n:Person {name: {i}}) RETURN n`,
+			Parameters: Props{"i": i},
+			Result:     &[]Node{},
+		}
+		qs = append(qs, &cq0)
+		nodes = append(nodes, i)
+		cq1 := CypherQuery{
+			Statement:  `MATCH a:Person, b:Person WHERE a.name = {i} AND b.name = {k} CREATE a-[r:Knows {name: {i}}]->b RETURN id(r)`,
+			Parameters: Props{"i": i, "k": i - 1},
+			Result:     &[]Relationship{},
+		}
+		qs = append(qs, &cq1)
+		rels = append(rels, i)
+	}
+	err := db.CypherBatch(qs)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.StopTimer()
+	//
+	// Cleanup
+	//
+	qs = []*CypherQuery{}
+	for _, r := range rels {
+		cq := CypherQuery{
+			Statement:  `MATCH ()-[r:Knows]->() WHERE r.name = {i} DELETE r`,
+			Parameters: Props{"i": r},
+		}
+		qs = append(qs, &cq)
+	}
+	for _, n := range nodes {
+		cq := CypherQuery{
+			Statement:  `MATCH n:Person WHERE n.name = {i} DELETE n`,
+			Parameters: Props{"i": n},
+		}
+		qs = append(qs, &cq)
+	}
+	err = db.CypherBatch(qs)
+	if err != nil {
+		b.Fatal(err)
+	}
 }
