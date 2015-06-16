3669baaaafa8638aaa8375528e8b3f8a39c550e1
AmirSaber <amirsaber@AmirSabers-MacBook-Pro.local>
2015-06-16 15:46:39 -0400

    add authentication

--- connect.go
+++ connect.go
@@ -7,6 +7,7 @@
 package neoism
 
 import (
+	"encoding/base64"
 	"net/http"
 	"net/url"
 	"strings"
@@ -16,6 +17,33 @@ import (
 
 // Connect setups parameters for the Neo4j server
 // and calls ConnectWithRetry()
+func ConnectWithAuth(uri string, username string, password string) (*Database, error) {
+	h := http.Header{}
+	h.Add("User-Agent", "neoism")
+	if username != "" && password != "" {
+		auth := base64.StdEncoding.EncodeToString([]byte(username+":"+password))
+		h.Add("Authorization", "Basic " + auth)
+	}
+	db := &Database{
+		Session: &napping.Session{
+			Header: &h,
+		},
+	}
+
+	// trailing slash is important, check if it's not there and add it
+	if !strings.HasSuffix(uri, "/") {
+		uri += "/"
+	}
+	parsedURL, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+	if parsedURL.User != nil {
+		db.Session.Userinfo = parsedURL.User
+	}
+	return connectWithRetry(db, parsedURL, 0)
+}
+
 func Connect(uri string) (*Database, error) {
 	h := http.Header{}
 	h.Add("User-Agent", "neoism")
